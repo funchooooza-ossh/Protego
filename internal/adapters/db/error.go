5b21d500ac7d8af341cc8e0cfdf4dd1ba43e7d4b
funchooooza-ossh <uroddaunich@gmail.com>
2025-06-06 21:57:49 +1000

    upd: ParseDBError fix

--- internal/adapters/db/error.go
+++ internal/adapters/db/error.go
@@ -6,13 +6,10 @@ import (
 	"errors"
 	"net"
 	"reflect"
-	"regexp"
 
 	e "github.com/funchooooza-ossh/protego/internal/errors"
 	"github.com/jackc/pgx"
 )
-
-var sqlstateRegex = regexp.MustCompile(`SQLSTATE (\d{5})`)
 
 func ParseDBError(err error, origin string) error {
 	if err == nil {
