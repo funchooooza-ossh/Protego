cae1a290a73d5afc6dda008e4ca0e05a5e2b3e2c
funchooooza-ossh <uroddaunich@gmail.com>
2025-06-06 12:26:40 +1000

    upd: role repo, user service

--- internal/db/role.queries.go
+++ internal/db/role.queries.go
@@ -2,8 +2,6 @@ package db
 
 import (
 	"context"
-	"database/sql"
-	"fmt"
 
 	"github.com/jackc/pgx/v5/pgtype"
 )
@@ -12,9 +10,6 @@ func (q *UQueries) GetRoleByID(ctx context.Context, id pgtype.UUID) (Role, error
 	row := q.db.QueryRow(ctx, getRoleByID, id)
 	var i Role
 	err := row.Scan(&i.ID, &i.Code)
-	if err != nil && err.Error() == "no rows in result set" {
-		return i, fmt.Errorf("%w", sql.ErrNoRows)
-	}
 	return i, err
 }
 
@@ -22,9 +17,6 @@ func (q *UQueries) GetRoleByCode(ctx context.Context, code string) (Role, error)
 	row := q.db.QueryRow(ctx, getRoleByCode, code)
 	var i Role
 	err := row.Scan(&i.ID, &i.Code)
-	if err != nil && err.Error() == "no rows in result set" {
-		return i, fmt.Errorf("%w", sql.ErrNoRows)
-	}
 	return i, err
 }
 
